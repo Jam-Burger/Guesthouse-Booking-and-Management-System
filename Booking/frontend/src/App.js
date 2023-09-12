b15dddabe35d30061778b00ae0d6302bc3b2dea1
Jam-Burger <jay40793@gmail.com>
2023-09-12 23:25:59 +0530

    add mongoDB connection

--- Booking/frontend/src/App.js
+++ Booking/frontend/src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+import "./App.css";
+import { Navbar, Container } from "react-bootstrap";
+const App = () => (
+  <>
+    <Navbar className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            Signed in as: <a href="#login">Mark Otto</a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  </>
+);
 
 export default App;
