d5dea0968eae7106472c956b35cd35e6ee7a2ca9
Km_ <kirtan.mevada2020@gmail.com>
2023-11-27 06:30:25 +0530

    All Booking Backen Changes

--- Booking/backend/routes/users.route.js
+++ Booking/backend/routes/users.route.js
@@ -57,33 +57,6 @@ router.get("/:id", async (req, res) => {
 const upload = multer();
 router.patch("/", upload.single('picture'), async (req, res) => {
   try {
-<<<<<<< HEAD
-    let updates = JSON.parse(req.body.profileData);
-    if (req.file) {
-      const { webContentLink } = await uploadFile(req.file);
-      console.log(webContentLink);
-      updates = { ...updates, profilePic: webContentLink };
-    }
-    const findObj = { _id: updates._id };
-    let data = await User.findOneAndUpdate(findObj, updates);
-
-    if (!data) {
-      res.status(404).json({
-        msg: "failure",
-        error: "data not found",
-      });
-    } else {
-      const token = jwt.sign({ currentUser: data }, process.env.JWT_SECRET, {
-        expiresIn: "1h",
-      });
-      res.cookie("currentUserToken", token, {
-        httpOnly: true,
-        maxAge: 60 * 60 * 1000,
-      });
-      res.json({ msg: "success", data });
-      console.log("data updated successfully");
-    }
-=======
     let updatedData = JSON.parse(req.body.profileData);
     if (req.file) {
       const { webContentLink } = await uploadFile(req.file);
@@ -104,7 +77,6 @@ router.patch("/", upload.single('picture'), async (req, res) => {
     });
     res.json({ msg: "success", data: updatedData });
     console.log("data updated successfully");
->>>>>>> 1a80ac3a634119659f124c11cc8c588aac25157e
   } catch (e) {
     console.log(e);
     res.status(500).json({
@@ -122,10 +94,6 @@ router.post("/signup", async (req, res) => {
     const hashedPassword = await bcrypt.hash(plainPassword, salt);
     const user = new User({ ...data, password: hashedPassword });
     console.log(plainPassword);
-<<<<<<< HEAD
-=======
-
->>>>>>> 1a80ac3a634119659f124c11cc8c588aac25157e
     await user.save();
     const token = jwt.sign({ currentUser: user }, process.env.JWT_SECRET, {
       expiresIn: "1h",
@@ -166,11 +134,8 @@ router.post("/login", async (req, res) => {
             expiresIn: "1h",
           });
           res.cookie("currentUserToken", token, {
-<<<<<<< HEAD
-=======
             secure: process.env.NODE_ENV !== "development",
             sameSite: process.env.NODE_ENV !== "development" ? 'none' : 'lax',
->>>>>>> 1a80ac3a634119659f124c11cc8c588aac25157e
             httpOnly: true,
             maxAge: 60 * 60 * 1000,
           });
