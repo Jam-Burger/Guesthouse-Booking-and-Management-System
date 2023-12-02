945745dcff67786834705bdb470d97c318618c54
Km_ <kirtan.mevada2020@gmail.com>
2023-12-02 19:22:57 +0530

    Change images for Welcome and Hotels Page

--- Booking/frontend/src/pages/WelcomePage.jsx
+++ Booking/frontend/src/pages/WelcomePage.jsx
@@ -25,12 +25,12 @@ const WelcomePage = () => {
             />
             <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-black bg-fixed opacity-50">
               <div className="opacity-100 flex-col flex h-full items-center justify-center">
-                <h1 className="opacity-100 mb-4 tracking-tight text-white textFrontf display-1">
+                <h1 className="opacity-100 mb-4 tracking-tight text-center text-white textFrontf display-1">
                   <b>
                     beyondSky
                   </b>
                 </h1>
-                <h1 className="opacity-100 mb-4 italic font-weight-light tracking-tight text-white textFrontf display-3">
+                <h1 className="opacity-100 mb-4 italic font-weight-light text-center tracking-tight text-white textFrontf display-3">
                     The sky is Not the Limit!
                 </h1>
                 <button type="button" onClick={scrollBottom} className="opacity-100 flex-col flex content-end items-end "><FaChevronDown className="dwnIcn" /></button>
@@ -41,7 +41,7 @@ const WelcomePage = () => {
             <h1 className="mb-6 display-3 text-center pt-5 px-5 pb-1 text-black cursive">
               Welcome to our Hotel!
             </h1>
-            <div className="flex flex-row items-center w-full justify-center pt-5 pr-10 pb-1 pl-10 lg:pt-5 lg:flex-row">
+            <div className="flex flex-row items-center w-full justify-center pt-5 pr-10 pb-3 pl-10 lg:pt-5 lg:flex-row">
               <hr className="h-0.5 my-4 mx-4 flex-row rounded w-48 bg-gray-200 border-0 md:my-10 dark:bg-gray-400" />
               <h2 className="mb-6 display-5 italic text-center py-3 px-6 text-black cursive">
                 Where accommodation meets expection.
@@ -58,7 +58,7 @@ const WelcomePage = () => {
                 className="absolute flex flex-col justify-center items-center content-center  bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed"
               >
                 <div className="maskingText w-full ">
-                  <h1 className="text-white text-center display-5 font-bold ">
+                  <h1 className="text-white text-center display-5 cursive font-bold ">
                     Want to book a Hotel?
                   </h1>
                   <button
